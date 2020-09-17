aebbde3e323071543321a55b360b472b731e933f
wvogel00 <eagle.hokkaido2317@gmail.com>
2020-09-17 17:31:22 +0900

    型の設計を進めた

--- src/Quail/Types.hs
+++ src/Quail/Types.hs
@@ -1,13 +1,34 @@
 module Quail.Types where
 
-data Music = GSound [Note] | FSound [Note]
+-- 楽譜
+data MusicalScore = GSound [KeySignature] [Bar]
+    | FSound [KeySignature] [Bar]
     deriving (Eq, Show)
 
-data Note = Note Scale Longth
+-- 小節
+type Bar = [Note]
 
-type Octave = Int
+-- 音符
+data Note = Note Scale (Maybe Sign) Octave Longth [Dot]
 
-data  Scale = C | CS | D | DS | E | F | FS | G | GS | A | AS | B
+-- 音階
+data  Scale = Rest | C | CS | D | DS | E | F | FS | G | GS | A | AS | B
     deriving (Eq, Show, Enum)
 
+-- オクターブ
+type Octave = Int
+
+-- 周波数
 type Freq = Float
+
+-- 音符長．全音符から32分音符まで
+data Longth = Full | L2 | L4 | L8 | L16 | L32 deriving (Eq, Show)
+
+-- 付点
+data Dot = Dot deriving (Eq, Show)
+
+-- 調号
+type KeySignature = (Scale, Sign)
+
+data Sign = Sharp | DoubleSharp | Flat | DoubleFlat | Natural
+    deriving (Eq, Show)
