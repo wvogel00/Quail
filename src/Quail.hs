c2bfc57789c693eb83507fa8bd6f8ea39022c2d2
wvogel00 <eagle.hokkaido2317@gmail.com>
2020-09-17 17:28:29 +0900

    型の設計を進めた

--- src/Quail.hs
+++ src/Quail.hs
@@ -7,7 +7,7 @@ play :: FilePath -> IO ()
 play path = callProcess "ffplay"
     ["-f", "f32le", "-ar", show sampleRate, "-showmode", "1", path]
 
-save :: Music -> IO ()
+-- save :: Music -> IO ()
 
 
 
@@ -19,3 +19,44 @@ toScale fq = (scale , octave) where
     findScale (a,b) = if a <= fq && fq <= b then 
 
 a0 = 440 :: Freq -- base sound : 440Hz (A)
+
+toFreq :: Note -> Freq
+toFreq n = 
+
+sharp :: Scale -> Scale
+sharp Rest = Rest
+sharp C = CS
+sharp CS = D
+sharp D = DS
+sharp DS = E
+sharp E = F
+sharp F = FS
+sharp FS = G
+sharp G = GS
+sharp GS = A
+sharp A = AS
+sharp AS = B
+sharp B = C
+
+-- double sharp
+sharp2 = sharp.sharp
+
+flat :: Scale -> Scale
+flat Rest = Rest
+flat C = B
+flat CS = C
+flat D = CS
+flat DS = D
+flat E = DS
+flat F = E
+flat FS = F
+flat G = FS
+flat GS = G
+flat A = GS
+flat AS = A
+flat B = AS
+
+flat2 = flat.flat
+
+len :: Longth -> [Dot] -> Float
+len l ds = len' l ++ sum (map (\v -> 1/2^v) $take (length ds) [1..])
