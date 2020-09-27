c1b1c6984f2adf9d21c805ebf8f9b5d69cf94c1c
wvogel00 <eagle.hokkaido2317@gmail.com>
2020-09-27 23:24:14 +0900

    周波数取得関数追加，リファクタリング

--- src/Quail/Utils.hs
+++ src/Quail/Utils.hs
@@ -1,8 +1,60 @@
 module Quail.Utils where
 
-import Quail.Types
+
+import System.Process (callCommand, callProcess)
 import Data.List
 import Data.Maybe
+import Quail.Types
+
+
+sampleRate = 44100
+
+initMusicalScore = MusicalScore (Metronome 60) []
+                    [(GClef,[initNote{len = (L16,[])},initNote{len = (L2,[])}])]
+
+initNote = Note
+    { index = 0
+    , scale = C
+    , sign = None
+    , oct = 4
+    , len = (L4,[])
+    , str = Nothing
+    , varStr = Nothing
+    , tempStr = []
+    , isVibrato = False
+    , tie = Nothing
+    , slur = Nothing
+    }
+
+playFile :: FilePath -> IO ()
+playFile path = callProcess "ffplay"
+    ["-f", "f32le", "-ar", show sampleRate, "-showmode", "1", path]
+
+
+noteSound :: Note -> Freq
+noteSound n = let r = 1.059463094
+                  a0 = 440 :: Freq
+              in a0/2**(fromIntegral $ 4-oct n) * r**(fromIntegral $ scale n `sub` A)
+
+sub Rest _ = 0
+sub _ Rest = 0
+sub s1 s2 = f s1 - f s2
+    where
+    table = zip [C .. B] [-9,-8..2]
+    f s = fromJust $ lookup s table
+
+isFlip :: Note -> Bool
+isFlip n = scale n >= C && oct n >= 5
+
+soundLen :: Bar -> Float
+soundLen = sum.map noteLen
+
+noteLen :: Note -> Float
+noteLen n = (getLen l) + sum (map (\v -> 1/2^v) $take (length dots) [1..])
+    where
+        (l,dots) = len n
+        getLen l = (\v -> 1/2^v). fromJust . lookup l $ zip [Full .. ] [0,1..]
+
 
 applySign :: Sign ->  Scale -> Scale
 applySign None = id
@@ -14,39 +66,8 @@ applySign DoubleFlat = flat.flat
 
 sharp :: Scale -> Scale
 sharp Rest = Rest
-sharp C = CS
-sharp CS = D
-sharp D = DS
-sharp DS = E
-sharp E = F
-sharp F = FS
-sharp FS = G
-sharp G = GS
-sharp GS = A
-sharp A = AS
-sharp AS = B
-sharp B = C
+sharp s = fromJust . lookup s $ zip (B:[C ..]) $ [C ..]
 
 flat :: Scale -> Scale
 flat Rest = Rest
-flat C = B
-flat CS = C
-flat D = CS
-flat DS = D
-flat E = DS
-flat F = E
-flat FS = F
-flat G = FS
-flat GS = G
-flat A = GS
-flat AS = A
-flat B = AS
-
-noteLen :: Note -> Float
-noteLen n = (getLen l) + sum (map (\v -> 1/2^v) $take (length dots) [1..])
-    where
-        (l,dots) = len n
-
-getLen :: Length -> Float
-getLen l = (\v -> 1/2^v). fromJust . lookup l $ zip [Full .. ] [0,1..]
-
+flat s = fromJust . lookup s $ zip [C ..] $ B:[C ..]
