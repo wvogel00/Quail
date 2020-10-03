cd885c718b5b722b09be068a425d89d5b2f2e5e9
Vikrem <me@vikrem.com>
2020-10-03 04:13:12 -0400

    Fix sound never stopping

--- src/Quail/Audio.hs
+++ src/Quail/Audio.hs
@@ -10,6 +10,7 @@ import Data.IORef
 import qualified Data.Vector.Storable.Mutable as V
 import Quail.Utils (getFreq, noteLen)
 import Quail.Types
+import Data.Word
 
 sampleFreq = 48000
 
@@ -27,25 +28,32 @@ sinSamples = map f [0..] where
         where t = fromIntegral n /fromIntegral sampleFreq
     freq = 440
 
-audioCB :: IORef [Int16] -> SDL.AudioFormat sampleType -> V.IOVector sampleType -> IO()
-audioCB sound format buf = case format of
+audioCB :: IORef (Maybe Word8) -> IORef [Int16] -> SDL.AudioFormat sampleType -> V.IOVector sampleType -> IO()
+audioCB silence sound format buf = case format of
     SDL.Signed16BitLEAudio -> do
         sound' <- readIORef sound
+        silence' <- readIORef silence
         let n = V.length buf
-        zipWithM_ (V.write buf) [0..] $ take n sound'
+        case silence' of
+            Just s -> zipWithM_ (V.write buf) [0..] $ take n $ sound' ++ (repeat $ fromIntegral s)
+            Nothing -> zipWithM_ (V.write buf) [0..] $ take n $ sound'
         writeIORef sound $ drop n sound'
     _ -> putStrLn "データ形式が誤っています (Int16のみ対応)"
 
 openAudio :: IORef [Int16] -> IO SDL.AudioDevice
-openAudio sound = fst <$> SDL.openAudioDevice SDL.OpenDeviceSpec
-    { SDL.openDeviceFreq = SDL.Mandate sampleFreq
-    , SDL.openDeviceFormat = SDL.Mandate SDL.Signed16BitNativeAudio
-    , SDL.openDeviceChannels = SDL.Mandate SDL.Mono
-    , SDL.openDeviceSamples = 4096 * 2
-    , SDL.openDeviceCallback = audioCB sound
-    , SDL.openDeviceUsage = SDL.ForPlayback
-    , SDL.openDeviceName = Nothing
-    }
+openAudio sound = do
+    silence <- newIORef Nothing
+    (device, spec) <- SDL.openAudioDevice SDL.OpenDeviceSpec
+        { SDL.openDeviceFreq = SDL.Mandate sampleFreq
+        , SDL.openDeviceFormat = SDL.Mandate SDL.Signed16BitNativeAudio
+        , SDL.openDeviceChannels = SDL.Mandate SDL.Mono
+        , SDL.openDeviceSamples = 4096 * 2
+        , SDL.openDeviceCallback = audioCB silence sound
+        , SDL.openDeviceUsage = SDL.ForPlayback
+        , SDL.openDeviceName = Nothing
+        }
+    writeIORef silence $ Just $ SDL.audioSpecSilence spec
+    return device
 
 closeAudio :: SDL.AudioDevice -> IO ()
 closeAudio = SDL.closeAudioDevice
